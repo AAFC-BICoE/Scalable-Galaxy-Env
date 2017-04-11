802900757def87e27c6d0263c7bcf74da0a63b84
katbeaulieu <katherine.beaulieu014@gmail.com>
2017-04-11 11:17:29 -0400

    Add files via upload

--- myFirstCronJob.sh
+++ myFirstCronJob.sh
@@ -1,4 +1,4 @@
-
+echo $key_name
 min=1
 max=5
 
@@ -24,7 +24,7 @@ then
         webInternalAddress=$(ip route get 8.8.8.8 | awk '{print $NF; exit}')
 	port=$(crudini --get /home/ubuntu/galaxy/config/galaxy.ini server:web0 port)
         port=$((port + 1))
-        ansible-playbook --extra-vars "vmName=$new_name" --extra-vars "webAddress=$webAddress" --extra-vars "webInternalAddress=$webInternalAddress" --extra-vars "port=$port" --extra-vars "handlerid=$new_name" /home/ubuntu/playbook.yaml
+        ansible-playbook --extra-vars "vmName=$new_name" --extra-vars "webAddress=$webAddress" --extra-vars "webInternalAddress=$webInternalAddress" --extra-vars "port=$port" --extra-vars "handlerid=$new_name" --extra-vars "image_id=$image_id" --extra-vars "flavor_name=$flavor" --extra-vars "private_network=$private_network" --extra-vars "key_name=$key_name" --extra-vars "private_key_name=$private_key_name" --extra-vars "security_group=$security_group" /home/ubuntu/playbook.yaml
         exit 0
 fi
 i=0
@@ -75,7 +75,7 @@ if [[ "$flag2" == "True" ]]; then
         	webInternalAddress=$(ip route get 8.8.8.8 | awk '{print $NF; exit}')
         	port=$(crudini --get /home/ubuntu/galaxy/config/galaxy.ini server:${handlers[$((i-1))]} port)
         	port=$((port + 1))
-		ansible-playbook --extra-vars "vmName=$new_name" --extra-vars "webAddress=$webAddress" --extra-vars "webInternalAddress=$webInternalAddress" --extra-vars "port=$port" --extra-vars "handlerid=$new_name" --extra-vars "image_id=$image_id" --extra-vars "key_name=$key_name" --extra-vars "flavor_name=$flavor_name" --extra-vars "private_network=$private_network" /home/ubuntu/playbook.yaml
+		ansible-playbook --extra-vars "vmName=$new_name" --extra-vars "webAddress=$webAddress" --extra-vars "webInternalAddress=$webInternalAddress" --extra-vars "port=$port" --extra-vars "handlerid=$new_name" --extra-vars "image_id=$image_id" --extra-vars "key_name=$key_name" --extra-vars "flavor_name=$flavor" --extra-vars "private_network=$private_network" --extra-vars "private_key_name=$private_key_name" --extra-vars "security_group=$security_group" /home/ubuntu/playbook.yaml
 		echo "Increase vm count"
 	fi
 #result equals False so one of the VMs was empy last time
@@ -93,7 +93,7 @@ else
 			echo $vmName
 			if [ "${#handlers[@]}" -gt "$min" ];
 			then
-				ansible-playbook --extra-vars "handlerid=${handlers[$i]}" --extra-vars "vmName=$vmName" /home/ubuntu/delete.yaml 
+				ansible-playbook --extra-vars "key_name=$key_name" --extra-vars "private_network=$private_network" --extra_vars "image_id=$image_id" --extra-vars "handlerid=${handlers[$i]}" --extra-vars "vmName=$vmName" /home/ubuntu/delete.yaml 
 			cp handlers.txt handlers.temp.txt
 			sed '/$vmName/d' handlers.temp.txt >| handlers.txt
 			fi
