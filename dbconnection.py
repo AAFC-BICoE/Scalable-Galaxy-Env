d516e41a501aef064da59363bca9959972c1bdcc
katbeaulieu <katherine.beaulieu014@gmail.com>
2017-04-25 11:30:39 -0400

    Add files via upload

--- dbconnection.py
+++ dbconnection.py
@@ -3,15 +3,12 @@ from sqlalchemy import *
 from sqlalchemy.orm import *
 from sqlalchemy.exc import *
 import pdb
-#from sqlalchemy import create_engine, MetaData, Table
-#from sqlalchemy.orm import scoped_session, sessionmaker
 import sys
 from six import string_types
 import os
 galaxy_root = os.path.abspath( os.path.join( os.path.dirname( __file__ ), os.pardir ) )
 sys.path.insert( 1, os.path.join( galaxy_root, 'lib' ) )
 
-#sys.path.append("/home/ubuntu/galaxy/lib/")
 from galaxy.model import *
 from galaxy.model.mapping import init
 from galaxy.model.orm.scripts import get_config
@@ -19,29 +16,17 @@ import argparse
 parser = argparse.ArgumentParser(description="Handler id")
 parser.add_argument('-handler','--handler', help="Handler id")
 args = parser.parse_args()
-#from galaxy.util import bunch
-#sys.path.append("/home/ubuntu/galaxy/lib/galaxy/util")
-#import bunch
-#sys.path.append("/home/ubuntu/galaxy/lib/galaxy/")
-#sys.path.append("/home/ubuntu/galaxy/lib/")
-#import galaxy.model
 
-#model = bunch.Bunch()
 config_file = "/home/ubuntu/galaxy/config/galaxy.ini"
 config = ConfigParser.ConfigParser()
 config.read(config_file)
 
+#Get database connection
 dburl = config.get('app:main','database_connection')
 sa_session = init( '/tmp/', dburl).context
 result = sa_session.query(galaxy.model.Job).get(int('1'))
-#cutoff_time = 10000
-#jobs = sa_session.query(galaxy.model.Job) \
-#                               .filter( and_( galaxy.model.Job.table.c.update_time < cutoff_time, 
-#                                              or_(galaxy.model.Job.state == galaxy.model.Job.states.NEW,
-#                                                    galaxy.model.Job.state == galaxy.model.Job.states == QUEUED,
-#                                                    galaxy.model.Job.state == galaxy.model.Job.states.RUNNING, 
-#	                                            galaxy.model.Job.state == galaxy.model.Job.states.UPLOAD ) ) )  
-#print sa_session.query(galaxy.model.Job).get
+
+#Get all jobs that are in one of the following states: new, queued, running, upload
 jobsNEW = sa_session.query(galaxy.model.Job).filter(galaxy.model.Job.state == 'new')
 jobsQUEUED = sa_session.query(galaxy.model.Job).filter(galaxy.model.Job.state == 'queued')
 jobsRUNNING = sa_session.query(galaxy.model.Job).filter(galaxy.model.Job.state == 'running')
@@ -54,12 +39,7 @@ resultQUEUED = sa_session.execute(jobsQUEUED)
 resultRUNNING = sa_session.execute(jobsRUNNING)
 resultUPLOAD = sa_session.execute(jobsUPLOAD)
 
-#print str(resultOK.job_handler)
-#for job in resultOK:
-	#pdb.set_trace()
-#	print str(job.items()[-1][-1])
-#	break
-
+#For the specified handler, check if there is a job in one of the following states: new, queued, running, upload
 handler = args.handler
 flagNew = False
 for job in resultNEW:
@@ -85,13 +65,4 @@ if flagNew or flagQueued or flagRunning or flagUpload:
 	print True		
 else:
 	print False
-#engine = create_engine( dburl )
-#metadata = MetaData( engine )
-#sa_session = scoped_session( sessionmaker( bind=engine, autoflush=False, autocommit=True ) )
-#model.Job = galaxy.model.Job
-#print dir(galaxy.model.Job)
-#model.Job.table = Table("job", metadata, autoload=True)
-#print dir(model.Job)
-#table = sa_session.query(model.Job).get('1')
-
 	
---
+++ jobChecker.sh
@@ -0,0 +1,102 @@
+min=1
+max=5
+output=$(grep -e "server:handler.*" /home/ubuntu/galaxy/config/galaxy.ini | wc -l)
+i=0
+
+if [ $output -eq 1 ];
+then 
+	iniHandlers=$(grep -o -e "server:handler.*" /home/ubuntu/galaxy/config/galaxy.ini | rev | cut -c 2- | rev) 
+else
+	iniHandlers=( $(grep -o -e "server:handler.*" /home/ubuntu/galaxy/config/galaxy.ini | rev | cut -c 2- | rev) )
+fi
+
+for k in "${iniHandlers[@]}"
+do
+	k=${k#server:}
+	handlers[$i]=$k
+	i=$(( $i+1 ))
+done
+
+handler="handler"
+
+if [ ${#handlers[@]} -eq 0 ];
+then
+	NEW_UUID=$(uuidgen)
+        new_name=$handler-$NEW_UUID
+        echo $new_name >> handlers.txt
+        webAddress=$(crudini --get /home/ubuntu/galaxy/config/galaxy.ini server:main host)
+        webInternalAddress=$(ip route get 8.8.8.8 | awk '{print $NF; exit}')
+	port=$(crudini --get /home/ubuntu/galaxy/config/galaxy.ini server:web0 port)
+        port=$((port + 1))
+        ansible-playbook --extra-vars "vmName=$new_name" --extra-vars "webAddress=$webAddress" --extra-vars "webInternalAddress=$webInternalAddress" --extra-vars "port=$port" --extra-vars "handlerid=$new_name" --extra-vars "image_id=$image_id" --extra-vars "flavor_name=$flavor" --extra-vars "private_network=$private_network" --extra-vars "key_name=$key_name" --extra-vars "private_key_name=$private_key_name" --extra-vars "security_group=$security_group" /home/ubuntu/handlerCreation.yaml
+        exit 0
+fi
+i=0
+output=0
+flag="True"
+#first check of whether or not handler has job on it
+while [ $output -eq 0 ];
+do 
+	result=$(python /home/ubuntu/galaxy/scripts/dbconnection.py -handler=${handlers[$i]})
+   	if [[ "$result" == "False" ]];then
+		flag="False"
+	fi
+	i=$(( $i + 1 ))
+	crudini --get /home/ubuntu/galaxy/config/galaxy.ini server:${handlers[$i]} host
+	output=$?
+done
+
+#sleep 3m
+
+output=0
+i=0
+flag2="True"
+
+while [ $output -eq 0 ];
+do
+	result2=$(python /home/ubuntu/galaxy/scripts/dbconnection.py --handler=${handlers[$i]})
+	if [[ "$result2" == "False" ]]; then
+		flag2="False"
+	fi
+	i=$(( $i + 1 ))
+	crudini --get /home/ubuntu/galaxy/config/galaxy.ini server:${handlers[$i]} host
+	output=$?
+done
+
+if [[ "$flag2" == "True" ]]; then
+	if [ "${#handlers[@]}" -lt "$max" -a "${#handlers[@]}" -ge "$min" ];
+	then
+		#launch ansible creation playbook
+		NEW_UUID=$(uuidgen)
+                new_name=$handler-$NEW_UUID
+		echo $new_name >> handlers.txt
+        	webAddress=$(crudini --get /home/ubuntu/galaxy/config/galaxy.ini server:main host) 
+        	webInternalAddress=$(ip route get 8.8.8.8 | awk '{print $NF; exit}')
+        	port=$(crudini --get /home/ubuntu/galaxy/config/galaxy.ini server:${handlers[$((i-1))]} port)
+        	port=$((port + 1))
+		ansible-playbook --extra-vars "vmName=$new_name" --extra-vars "webAddress=$webAddress" --extra-vars "webInternalAddress=$webInternalAddress" --extra-vars "port=$port" --extra-vars "handlerid=$new_name" --extra-vars "image_id=$image_id" --extra-vars "key_name=$key_name" --extra-vars "flavor_name=$flavor" --extra-vars "private_network=$private_network" --extra-vars "private_key_name=$private_key_name" --extra-vars "security_group=$security_group" /home/ubuntu/handlerCreation.yaml
+		echo "Increase vm count"
+	fi
+#result equals False so one of the VMs was empy last time
+else	
+	i=0
+	result3="True"
+	while [[ "$result3" == "True" ]];do
+		result3=$(python /home/ubuntu/galaxy/scripts/dbconnection.py --handler=${handlers[$i]})
+		if [[ "$result3" == "False" ]];then
+			emptyhost=$(crudini --get /home/ubuntu/galaxy/config/galaxy.ini server:${handlers[$i]} host)
+			echo "Delete a vm"
+			echo ${handlers[$i]}
+ 			vmName=`grep -e "${handlers[$i]}" handlers.txt`
+			if [ "${#handlers[@]}" -gt "$min" ];
+			then
+				ansible-playbook --extra-vars "key_name=$key_name" --extra-vars "private_network=$private_network" --extra-vars "image_id=$image_id" --extra-vars "handlerid=${handlers[$i]}" --extra-vars "vmName=$vmName" /home/ubuntu/handlerDeletion.yaml 
+			cp handlers.txt handlers.temp.txt
+			sed '/$vmName/d' handlers.temp.txt >| handlers.txt
+			fi
+		fi
+		i=$(( $i + 1 ))
+	done
+fi
+
+
