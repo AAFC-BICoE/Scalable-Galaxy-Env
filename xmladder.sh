092f6198b80bc1143022fb95eff82b0b78b5359c
katbeaulieu <katherine.beaulieu014@gmail.com>
2017-05-12 10:30:15 -0400

    Delete xmladder.sh

--- xmladder.sh
+++
@@ -1,6 +0,0 @@
-xmlstarlet ed \
-  --subnode /job_conf/handlers --type elem -n handler \
-  --subnode '/job_conf/handlers/handler[last()]' --type attr -n id -v $1 \
-  --subnode '/job_conf/handlers/handler[last()]' --type attr -n tags -v handlers \
-  $2 >| $3
-
